a0ebd001ff4668db7ae45ecbc0fe058544051f2b
marcobusacca <marcobusacca01@gmail.com>
2023-07-21 17:12:28 +0200

    [FIX]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
         <!-- Styles -->
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
+        @vite('resources/js/app.js')
     </head>
     <body>
     </body>

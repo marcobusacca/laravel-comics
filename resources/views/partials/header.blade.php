a13b836f9e63fef73e0c0d414ed92b5d5fe4ce4b
marcobusacca <marcobusacca01@gmail.com>
2023-07-21 17:58:28 +0200

    [FEAT] header top bar layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,2 +1,13 @@
 <header>
+    <!-- Header Top Bar -->
+    <div class="container-fluid header-top-bar">
+        <div class="container">
+            <div class="row">
+                <ul class="primary-color m-1">
+                    <li>DC POWER&#8480; VISA&#174;</li>
+                    <li>ADDITIONAL DC SITES</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </header>

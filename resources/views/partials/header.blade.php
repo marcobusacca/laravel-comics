abbe351de2949f460461d298c1ae076fda0aeec1
marcobusacca <marcobusacca01@gmail.com>
2023-07-21 18:06:06 +0200

    [FEAT] header top bar added fa-caret-down

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,10 @@
             <div class="row">
                 <ul class="primary-color m-1">
                     <li>DC POWER&#8480; VISA&#174;</li>
-                    <li>ADDITIONAL DC SITES</li>
+                    <li>
+                        <span>ADDITIONAL DC SITES</span>
+                        <i class="fa-solid fa-caret-down"></i>
+                    </li>
                 </ul>
             </div>
         </div>

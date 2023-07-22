8e485dfadc11aaf151833358b45c5b9c66200eca
marcobusacca <marcobusacca01@gmail.com>
2023-07-22 15:46:30 +0200

    [FEAT] header bottom bar layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,13 +3,69 @@
     <div class="container-fluid header-top-bar">
         <div class="container">
             <div class="row">
-                <ul class="primary-color m-1">
-                    <li>DC POWER&#8480; VISA&#174;</li>
-                    <li>
-                        <span>ADDITIONAL DC SITES</span>
+                <div class="col-12">
+                    <ul class="primary-color m-1">
+                        <li>DC POWER&#8480; VISA&#174;</li>
+                        <li>
+                            <span>ADDITIONAL DC SITES</span>
+                            <i class="fa-solid fa-caret-down"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header Bottom Bar -->
+    <div class="container header-bottom-bar">
+        <div class="row">
+            <!-- Header Logo Col -->
+            <div class="col-2 py-4">
+                <!-- Header Logo -->
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <!-- Header NavBar Col -->
+            <div class="col-8">
+                <!-- Header NavBar List -->
+                <ul class="d-flex w-100 h-100">
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">CHARACTERS</a>
+                    </li>
+                    <li class="active h-100">
+                        <a href="{{ Route('home') }}">COMICS</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">MOVIES</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">TV</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">GAMES</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">COLLECTIBLES</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">VIDEOS</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">FANS</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">NEWS</a>
+                    </li>
+                    <li class="h-100">
+                        <a href="{{ Route('home') }}">SHOP</a>
                         <i class="fa-solid fa-caret-down"></i>
                     </li>
                 </ul>
+            </div>
+            <div class="col-2 d-flex align-items-center px-5">
+                <!-- Header NavBar SearchBar -->
+                <div class="search-bar">
+                    <span class="fw-bold">Search</span>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
             </div>
         </div>
     </div>

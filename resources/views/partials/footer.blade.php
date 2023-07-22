e8878b3e7feb5ffa46331f9da835049267e5e63d
marcobusacca <marcobusacca01@gmail.com>
2023-07-22 17:25:42 +0200

    [FEAT] footer menu layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,35 +8,166 @@
                     <!-- Banner Image -->
                     <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="banner-image">
                     <!-- Banner Text -->
-                    <span>DIGITAL COMICS</span>
+                    <a href="{{ Route('home') }}">DIGITAL COMICS</a>
                 </div>
                 <!-- Banner Col -->
                 <div class="banner-col">
                     <!-- Banner Image -->
                     <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="banner-image">
                     <!-- Banner Text -->
-                    <span>DC MERCHANDISE</span>
+                    <a href="{{ Route('home') }}">DC MERCHANDISE</a>
                 </div>
                 <!-- Banner Col -->
                 <div class="banner-col">
                     <!-- Banner Image -->
                     <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="banner-image">
                     <!-- Banner Text -->
-                    <span>SUBSCRIPTION</span>
+                    <a href="{{ Route('home') }}">SUBSCRIPTION</a>
                 </div>
                 <!-- Banner Col -->
                 <div class="banner-col">
                     <!-- Banner Image -->
                     <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="banner-image">
                     <!-- Banner Text -->
-                    <span>COMIC SHOP LOCATOR</span>
+                    <a href="{{ Route('home') }}">COMIC SHOP LOCATOR</a>
                 </div>
                 <!-- Banner Col -->
                 <div class="banner-col">
                     <!-- Banner Image -->
                     <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="banner-image">
                     <!-- Banner Text -->
-                    <span>DC POWER VISA</span>
+                    <a href="{{ Route('home') }}">DC POWER VISA</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Menu Container -->
+    <div class="container-fluid menu-container">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Menu Col -->
+                <div class="col-6">
+                    <div class="row flex-wrap">
+                        <div class="col-4">
+                            <!-- List Title -->
+                            <h3>DC COMICS</h3>
+                            <!-- Menu List -->
+                            <ul>
+                                <li>
+                                    <a href="{{ Route('home') }}">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">TV</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Games</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">News</a>
+                                </li>
+                            </ul>
+                            <!-- List Title -->
+                            <h3>SHOP</h3>
+                            <!-- Menu List -->
+                            <ul>
+                                <li>
+                                    <a href="{{ Route('home') }}">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <!-- List Title -->
+                            <h3>DC</h3>
+                            <!-- Menu List -->
+                            <ul>
+                                <li>
+                                    <a href="{{ Route('home') }}">Terms Of Use</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Privacy policy (New)</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Ad Choices</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Subscriptions</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Talent Workshops</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">CPSC Certificates</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Ratings</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <!-- List Title -->
+                            <h3>SITES</h3>
+                            <!-- Menu List -->
+                            <ul>
+                                <li>
+                                    <a href="{{ Route('home') }}">DC</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">MAD Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">DC Universe</a>
+                                </li>
+                                <li>
+                                    <a href="{{ Route('home') }}">DC Power Visa</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <p>
+                            All Site Content TM and 2020 DC Entertainment, unless otherwise
+                            <a href="{{ Route('home') }}">noted here.</a>
+                            All rights reserved.
+                            <br>
+                            <a href="{{ Route('home') }}">Cookies Settings</a>
+                        </p>
+                    </div>
+                </div>
+                <!-- Footer Background Logo Col -->
+                <div class="col-6">
+                    <div class="row">
+                        <div class="background-logo-col">
+                            <!-- Background Logo -->
+                            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="dc-background-logo">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

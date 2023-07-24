78415cdb48292ef54aee31f1c1acb67e5223d012
marcobusacca <marcobusacca01@gmail.com>
2023-07-24 15:33:45 +0200

    [FEAT] show page comic banner layout

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,6 +5,16 @@
         <div class="show-page">
             <!-- Jumbotron Container -->
             <div class="container-fluid jumbotron-container"></div>
+            <!-- Comic Banner Container -->
+            <div class="container-fluid comic-banner-container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <img src="{{ $comic['thumb'] }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 @endsection

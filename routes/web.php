5ea415eba4707a493b1a789691fff8252728e366
marcobusacca <marcobusacca01@gmail.com>
2023-07-24 15:04:20 +0200

    [FEAT] folder comics and index page

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $comics = config('db.comics');
-    return view('home', compact('comics'));
+    return view('comics.index', compact('comics'));
 })->name('home');
